97441f1b171fa68f051911e111b457478df78375
Devinder Kumar <devinder.9878874376@gmail.com>
2023-01-11 20:19:56 +0530

    m

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let navBarToggle = document.getElementById("nav-bar-toggle");
 
 let navigationList = document.getElementById("navigation-link");
 navBarToggle.onmouseenter = function () {
-    navigationList.style.display = "block";
+    navigationList.style.display = "flex";
 }
 
 navBarToggle.onmouseleave = function () {
     navigationList.style.display = "none";
-    navigationList.onmouseenter = function () {
-        this.style.display = "block";
-    }
+}
+navigationList.onmouseenter = function () {
+    this.style.display = "flex";
+}
 
-    navigationList.onmouseleave = function () {
-        this.style.display = "none";
-    }
+navigationList.onmouseleave = function () {
+    this.style.display = "none";
 }
